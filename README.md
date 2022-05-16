3110ff3a03b95a7f0f4c3d55e5f79eca86575238
Jason Jacobson <jdesigns.jason@gmail.com>
2022-05-16 14:25:27 -0500

    feat(initial): Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Changelog Output
+
+A module created to convert a standard version changelog to a more readable ouput
+for a more product focused view.
+
+## Run
+```
+npm run generate
+
+or
+
+node index.js
+```
+
+## Options
+There are 4 options you can provide to the generate either as command line args or as a config.json file.
+
+> NOTE: Slack webhooks are secret URLS and should not allowed in the config file. Pass as an arg for safety.
+
+Link is built off the assumption that you are using jira. How this works is it looks for the ticket number inside of
+description of your commit and if it exists it will append that value to the end of the link property.
+
+### Config
+In the root directory where you run this command add a `config.json` file.
+
+```
+{
+	"writeOutput": false,
+	"fileName": "CHANGELOG.md",
+	"link": "https://some/path/to/"
+}
+```
+
+### Command line args
+Command line args are the same values as the config but allows one more option for slack webhook integration.
+
+Slack integration already uses the following value as a base so you just need to provide the secrets portion.
+`https://hooks.slack.com/services/`
+
+```
+npm run generate --writeOutput false --fileName CHANGELOG.md --link https://some/path/to/ --slack my/secret/ending/url
+```
+
+### Link
+As stated above the link is under the assumption you are using Jira.
+
+The regex pattern inside of commit descriptions is
+```
+/([a-zA-Z]+(-[0-9]+)+)/g
+```
+
+This is appended to the end of the link value you provide.
+
+Example
+```
+{
+	"link": "https://something.atlassian.net/browse/"
+}
+
+commit message
+feat(thing): POS-43 The coolest feature ever
+
+Output link value will be
+https://something.atlassian.net/browse/POS-43
+```
---
+++ index.js
@@ -0,0 +1,172 @@
+'use strict'
+const fs = require('fs')
+const axios = require('axios')
+const commandLineArgs = require('command-line-args')
+const optionDefinitions = [
+  { name: 'writeOutput', type: Boolean, defaultValue: false },
+  { name: 'fileName', type: String },
+  { name: 'link', type: String },
+	{ name: 'slack', type: String }
+]
+const options = commandLineArgs(optionDefinitions)
+const { writeOutput, inputFile, link, slack } = options
+let baseLink = link || 'https://bofcmportal.atlassian.net/browse/'
+let fileName = inputFile || 'CHANGELOG_TESTINGOUTPUT.md'
+let slackPath = slack || ''
+const config = 'config.json'
+let writeOutputFile = writeOutput || false
+let version = ''
+let title = ''
+let releaseDate = ''
+
+function checkForConfig() {
+	return new Promise((res, rej) => {
+		fs.readFile(`${__dirname}/${config}`, 'utf8', (err, config) => {
+			if (err) {
+				console.error('err', err)
+				rej(err)
+			}
+
+			try {
+				const parsed = JSON.parse(config);
+				if (parsed.writeOutput) {
+					writeOutputFile = parsed.writeOutput
+				}
+				if (parsed.fileName) {
+					fileName = parsed.fileName
+				}
+				if (parsed.link) {
+					baseLink = parsed.link
+				}
+			} catch (err) {
+				rej(err)
+			}
+	
+			if (config) {
+				fileData()
+				res();
+			}
+		})
+	})
+}
+
+async function fileData() {
+	if (!baseLink) {
+		console.error('Missing the baseLink value for links')
+		return
+	}
+
+	fs.readFile(`${__dirname}/${fileName}`, 'utf8', (err, data) => {
+		if (err) {
+			console.error('err', err)
+			return
+		}
+
+		if (data) {
+			createTitle(data)
+			createReleaseDate(data)
+			const issues = getCommitsFromVersion(data)
+			const issuesWLinks = createLinks(issues)
+			createOutputFile(issuesWLinks)
+		} else {
+			console.error('ERROR:: No data from file')
+		}
+	})
+}
+
+function createTitle(data) {
+	const headerRegex = /#([\s\S]*?)\n/g
+	const header = data.match(headerRegex)
+	title = header[0]
+}
+
+function createReleaseDate(data) {
+	const dateRegex = /(\(\d{4}-\d{2}-\d{2}\))/g
+	const date = data.match(dateRegex)
+	releaseDate= date[0]
+}
+
+function getCommitsFromVersion(data) {
+	const versionRegex = /\d.\d.\d/g
+	const versions = data.match(versionRegex)
+	// RegExp for \s\S needs additional slash as being stripped
+	version = versions[0]
+	const allCommitsRegex = RegExp(versions[0] + '([\\s\\S]*?)\]', 'gm')
+	const allCommits = data.match(allCommitsRegex)
+	const commitRegex = /\*\*([\s\S]*?)\n/g
+	const issues = []
+
+	for (let i = 0; i < allCommits.length; i++) {
+		const match = allCommits[i].match(commitRegex)
+
+		if (match && match.length > 0) {
+			for (let m = 0; m < match.length; m++) {
+				issues.push(match[m])
+			}
+		}
+	}
+
+	return issues;
+}
+
+function createLinks(issues) {
+	const output = []
+	const WDFRegex = /([a-zA-Z]+(-[0-9]+)+)/g
+
+	for (let i = 0; i < issues.length; i++) {
+		const issue = {
+			title: `• ${issues[i]}`
+		}
+		
+		if (WDFRegex.test(issues[i])) {
+			const match = issues[i].match(WDFRegex)
+			issue['link'] = `[[${match[0]}]](${baseLink}${match[0]})`
+		}
+
+		output.push(issue)
+	}
+
+	return output
+}
+
+function createOutputFile(data) {
+	let outputFile = `${title}${releaseDate}\n${version}\n\n${data.map(d => {
+		if (d.link) {
+			return d.title + d.link + '\n\n'
+		}
+		return d.title + '\n'
+	})}`;
+	outputFile = outputFile.replace(/,/g, '')
+	outputFile = outputFile.replace(/\*\*/g, '')
+	console.warn(outputFile)
+
+	if (slackPath) {
+		axios({
+			method: 'post',
+			url: `https://hooks.slack.com/services/${slackPath}`,
+			data: {
+				text: 'Changelog',
+				blocks: [
+					{
+						type: 'section',
+						text: {
+							type: 'mrkdwn',
+							text: outputFile
+						}
+					}
+				]
+			},
+			headers: {
+				'Content-type': 'application/json'
+			}
+		})
+	}
+
+	if (writeOutputFile) {
+		fs.writeFile(`${__dirname}/outputFile.txt`, outputFile, err => {
+			if (err) console.error('failed to write')
+		})
+	}
+}
+
+checkForConfig()
